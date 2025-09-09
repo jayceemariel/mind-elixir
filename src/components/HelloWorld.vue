edeb19a7192c774a01d2a15b9d9b4446abb80684
jayceemariel <62037782+jayceemariel@users.noreply.github.com>
2025-09-09 11:59:29 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="map"></div>
+</template>
+<script setup>
+import MindElixir from "mind-elixir";
+import "mind-elixir/style";
+import example from "mind-elixir/example";
+import { onMounted, ref } from "vue";
+
+const me = ref();
+onMounted(() => {
+  me.value = new MindElixir({
+    el: "#map",
+  });
+  me.value.init(example);
+});
+</script>
+<style lang="less" scoped>
+#map {
+  width: 100%;
+  height: 500px;
+}
+</style>
